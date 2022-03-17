35818a03e13046984122ac19580ddac6777e22e3
kristoferfannar <kristoferfannarb@gmail.com>
2022-03-17 19:11:49 +0000

    added implementation so that I can transfer songs between playlists

--- afmalis.py
+++ afmalis.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from secretsAPI import spotify_token, spotify_user_id
+from secretsAPI import spotify_token, spotify_user_id, python_playlist_id
 import pprint
 printer = pprint.PrettyPrinter()
 
@@ -25,15 +25,6 @@ def create_playlist():
     json_response = api_response.json() 
     print(json_response)
 
-# def find_playlist(playlistid):
-
-#     endpoint = 'https://api.spotify.com/v1/playlists/{playlist_id}'.format(playlist_id = playlistid)
-
-#     header = {'Authorization': 'Bearer {token}'.format(CLIENTSECRET), 'Content-Type':'application/json'}
-
-#     playlists_response = requests.get(endpoint, headers = header)
-#     json_playlists = playlists_response.json()
-#     print(json_playlists)
 
 
 
@@ -48,19 +39,39 @@ def get_spotify_uri(songname, artist):
     return response_json["tracks"]["items"][0]
 
 
-response_json = get_spotify_uri("ballin", "logic")
-
-response_list = response_json
-
-#print(response_list.keys())
-response_list["album"].pop("available_markets")
-for item in ["available_markets", "duration_ms", "disc_number", "explicit", "track_number"]:
-    response_list.pop(item)
-
 
 def find_playlist_from_link(playlistlink):
-    pass
+    sind = len('https://open.spotify.com/playlist/')
+    eind = playlistlink.find('?')
+    playlist_id = playlistlink[sind:eind]
 
+    query = f'https://api.spotify.com/v1/playlists/{playlist_id}'
+
+    header = {'Authorization': 'Bearer {}'.format(spotify_token), 'Content-Type':'application/json'}
+
+    playlists_response = requests.get(query, headers = header)
+    json_playlists = playlists_response.json()
+    print(json_playlists)
+
+    return json_playlists
+
+def all_songs_from_playlist_link(playlistlink): #have to deal with if len(songslist > 100)
+    json_playlist = find_playlist_from_link(playlistlink)
+    songslist = [i['track']['uri'] for i in json_playlist['tracks']['items']] #gives me the uris of all the playlist's tracks.
+
+    return songslist
+
+
+
+
+def add_from_oldplaylist_to_newplaylist(oldplaylist_link, newplaylist_id):
+    query = f'https://api.spotify.com/v1/playlists/{newplaylist_id}/tracks'
+    header = {'Content-Type':'application/json','Authorization': f'Bearer {spotify_token}'}
+    rdata = all_songs_from_playlist_link(oldplaylist_link)
+
+    response = requests.post(query, headers= header, data=json.dumps(rdata))
+    json_resp = response.json()
+    print(json_resp)
 
 
 
@@ -75,4 +86,22 @@ def print_dict(somedic, indent = ''):
 
 #print_dict(response_list)
 
-create_playlist()
+
+
+
+if __name__ == "__main__":
+    pass
+
+    # response_json = get_spotify_uri("ballin", "logic")
+    # response_list = response_json
+    # response_list["album"].pop("available_markets")
+    # for item in ["available_markets", "duration_ms", "disc_number", "explicit", "track_number"]:
+    #     response_list.pop(item)
+    #create_playlist()
+
+    # id_test = find_playlist_from_link('https://open.spotify.com/playlist/18STgHLE9PYjzXuhRuxB9H?si=e5e8eac266d74f56')
+
+    # jsonplaylist = all_songs_from_playlist_link('https://open.spotify.com/playlist/00y8K6estzrMcPVFCfPMaW?si=ZntEY-EdQHOTfTHqfqsBoQ&utm_source=copy-link')
+    # print(jsonplaylist)
+
+    add_from_oldplaylist_to_newplaylist('https://open.spotify.com/playlist/00y8K6estzrMcPVFCfPMaW?si=ZntEY-EdQHOTfTHqfqsBoQ&utm_source=copy-link',python_playlist_id)
